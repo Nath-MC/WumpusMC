9bdb7df3a62b9e8dee08c9c808cf26258f1bbd43
xYvwax <>
2024-05-12 16:05:28 +0400

    Removed unnecessary code for the event listener

--- src/discord/EventListener.js
+++ src/discord/EventListener.js
@@ -3,42 +3,29 @@ const fs = require("node:fs");
 const path = require("node:path");
 
 /**
- *
  * @param {Client} client
- * @param {Comm} interaction
  */
-
-async function startEventListener(client) {
-
+module.exports = (client) => {
   const foldersPath = path.join(__dirname, "events");
   const eventFolders = fs.readdirSync(foldersPath);
 
-  let eventsHandled = 0;
-
   for (const folder of eventFolders) {
-
     const eventPath = path.join(foldersPath, folder);
     const eventFiles = fs
       .readdirSync(eventPath)
       .filter((file) => file.endsWith(".js"));
 
     for (const file of eventFiles) {
-        
       const filePath = path.join(eventPath, file);
       const event = require(filePath);
 
       if ("name" in event && "execute" in event) {
-
         client.on(event.name, (...args) => event.execute(...args));
-        eventsHandled++;
-
       } else
         throw new Error(
           `${filePath} event is missing a property ! (name, execute)`
         );
     }
   }
-  console.log(`${eventsHandled} events were registered.`);
+  console.log(`${client.eventNames().filter(e => e !== "shardDisconnect").length} events were registered.`);
 }
-
-module.exports = startEventListener;
